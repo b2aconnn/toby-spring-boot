0dccb83a015f2b09aeeea962bc0526f1bf103efa
b2aconnn <b2aconnn@gmail.com>
2023-11-14 15:31:40 +0900

    [추가] 독립 실행형 스프링 어플리케이션 구현

---
+++ src/main/java/tobyspring/helloboot/ConfigurationFrontBeanController.java
@@ -0,0 +1,47 @@
+package tobyspring.helloboot;
+
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+@Configuration
+@ComponentScan
+public class ConfigurationFrontBeanController {
+    @Bean
+    public ServletWebServerFactory serverFactory() {
+        return new TomcatServletWebServerFactory();
+    }
+
+    @Bean
+    public DispatcherServlet dispatcherServlet() {
+        return new DispatcherServlet();
+    }
+
+    public void service(Class<?> applicationClass) {
+        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
+            @Override
+            protected void onRefresh() {
+                super.onRefresh();
+
+                ServletWebServerFactory serverFactory = this.getBean(ServletWebServerFactory.class);
+                DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
+                dispatcherServlet.setApplicationContext(this);
+
+                WebServer webServer = serverFactory.getWebServer(servletContext -> {
+                    servletContext.addServlet("dispatcherServlet", dispatcherServlet
+                    ).addMapping("/*");
+                });
+                webServer.start();
+            }
+        };
+
+        // Bean을 찾기 위해 구성 정보 클래스를 등록해줘야 함
+        applicationContext.register(applicationClass);
+        applicationContext.refresh(); // Spring Container를 초기화
+    }
+}
