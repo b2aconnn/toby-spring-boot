ccb6074bdf5577cf454defd9a630405241290a31
b2aconnn <b2aconnn@gmail.com>
2023-12-11 18:35:03 +0900

    [추가] 조건부 자동 구성

--- src/main/java/tobyspring/config/autoconfig/JettyWebServerConfig.java
+++ src/main/java/tobyspring/config/autoconfig/JettyWebServerConfig.java
@@ -1,31 +1,28 @@
 package tobyspring.config.autoconfig;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Condition;
-import org.springframework.context.annotation.ConditionContext;
-import org.springframework.context.annotation.Conditional;
-import org.springframework.core.type.AnnotatedTypeMetadata;
-import org.springframework.util.ClassUtils;
+import tobyspring.config.ConditionalMyClass;
 import tobyspring.config.MyAutoConfiguration;
 
 @MyAutoConfiguration
-// @Condition : 해당 Configuration 클래스에 있는 Bean들을 등록할 지 말 지 설정
-// Condition Interface를 구현한 클래스를 엘리먼트로 등록해야함
-@Conditional(JettyWebServerConfig.JettyCondition.class)
+// Custom Conditional
+@ConditionalMyClass("org.eclipse.jetty.server.Server")
 public class JettyWebServerConfig {
     @Bean("jettyWebServerFactory")
+    @ConditionalOnMissingBean
     public ServletWebServerFactory serverFactory() {
         return new JettyServletWebServerFactory();
     }
 
-    static class JettyCondition implements Condition {
-        @Override
-        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-            // ClassUtils.isPresent() : 현재 프로젝트에 특정 라이브러리 클래스가 존재하는 지 체크
-            return ClassUtils.isPresent("org.eclipse.jetty.server.Server",
-                    context.getClassLoader());
-        }
-    }
+//    static class JettyCondition implements Condition {
+//        @Override
+//        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+//            // ClassUtils.isPresent() : 현재 프로젝트에 특정 라이브러리 클래스가 존재하는 지 체크
+//            return ClassUtils.isPresent("org.eclipse.jetty.server.Server",
+//                    context.getClassLoader());
+//        }
+//    }
 }
--- src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
@@ -1,29 +1,28 @@
 package tobyspring.config.autoconfig;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Condition;
-import org.springframework.context.annotation.ConditionContext;
-import org.springframework.context.annotation.Conditional;
-import org.springframework.core.type.AnnotatedTypeMetadata;
-import org.springframework.util.ClassUtils;
+import tobyspring.config.ConditionalMyClass;
 import tobyspring.config.MyAutoConfiguration;
 
 @MyAutoConfiguration
-@Conditional(TomcatWebServerConfig.TomcatCondition.class)
+// Custom Conditional
+@ConditionalMyClass("org.apache.catalina.startup.Tomcat")
 public class TomcatWebServerConfig {
     @Bean("tomcatWebServerFactory")
+    @ConditionalOnMissingBean
     public ServletWebServerFactory serverFactory() {
         return new TomcatServletWebServerFactory();
     }
 
-    static class TomcatCondition implements Condition {
-        @Override
-        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-            // ClassUtils.isPresent() : 현재 프로젝트에 특정 라이브러리 클래스가 존재하는 지 체크
-            return ClassUtils.isPresent("org.apache.catalina.startup.Tomcat",
-                    context.getClassLoader());
-        }
-    }
+//    static class TomcatCondition implements Condition {
+//        @Override
+//        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+//            // ClassUtils.isPresent() : 현재 프로젝트에 특정 라이브러리 클래스가 존재하는 지 체크
+//            return ClassUtils.isPresent("org.apache.catalina.startup.Tomcat",
+//                    context.getClassLoader());
+//        }
+//    }
 }
