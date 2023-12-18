9d8711d882a6975828a6c5a40e74a1b8fa4a336e
b2aconnn <b2aconnn@gmail.com>
2023-12-18 15:13:47 +0900

    Revert "[수정] 조건부 자동 구성"
    This reverts commit 415e2f1c509e7ceabfbc13052ccf5bc82019ac6a.

--- src/main/java/tobyspring/config/MyOnClassCondition.java
+++ src/main/java/tobyspring/config/MyOnClassCondition.java
@@ -12,8 +12,6 @@ public class MyOnClassCondition implements Condition {
     public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
         Map<String, Object> attrs = metadata.getAnnotationAttributes(ConditionalMyClass.class.getName());
         String value = (String) attrs.get("value");
-
-        // 해당 라이브러리(value에 있는 값)가 있는 지 체크
         return ClassUtils.isPresent(value, context.getClassLoader());
     }
 }
